1aea3f90445c652651ea4d42b94dd58f18d5d1c7
CryptAxe <cryptaxe@gmail.com>
2024-04-22 21:38:40 -0700

    Update fast withdraw & net code for multi-chain support

--- ui/components/fast_withdraw/fast_withdraw.gd
+++ ui/components/fast_withdraw/fast_withdraw.gd
@@ -7,7 +7,6 @@ const PORT = 8382
 
 var invoice_address : String = ""
 
-
 func _ready() -> void:
 	multiplayer.connected_to_server.connect(_on_connected_to_withdrawal_server)
 	multiplayer.connection_failed.connect(_on_failed_connect_to_withdrawal_server)
@@ -28,7 +27,7 @@ func _on_connected_to_withdrawal_server() -> void:
 	
 func _on_disconnected_from_withdrawal_server() -> void:
 	if $"/root/Net".print_debug_net:
-		print("Disonnected to fast withdraw server")
+		print("Disonnected from fast withdraw server")
 	
 	$LabelConnectionStatus.text = "Not Connected"
 	
@@ -107,12 +106,14 @@ func _on_button_copy_address_pressed() -> void:
 func _on_button_invoice_paid_pressed() -> void:
 	# Tell the server we paid
 	var txid : String = $LineEditTXID.text
-	$"/root/Net".invoice_paid.rpc_id(1, txid, $SpinBoxAmount.value, $LineEditMainchainAddress.text)
+	var chain_name = $ChainSelectionButton.get_item_text($ChainSelectionButton.get_selected())
+	$"/root/Net".invoice_paid.rpc_id(1, chain_name, txid, $SpinBoxAmount.value, $LineEditMainchainAddress.text)
 
 
 func _on_button_request_invoice_pressed() -> void:
 	# Send fast withdraw request only to server
-	$"/root/Net".request_fast_withdraw.rpc_id(1, $SpinBoxAmount.value, $LineEditMainchainAddress.text)
+	var chain_name = $ChainSelectionButton.get_item_text($ChainSelectionButton.get_selected())
+	$"/root/Net".request_fast_withdraw.rpc_id(1, chain_name, $SpinBoxAmount.value, $LineEditMainchainAddress.text)
 
 
 func _on_button_connect_pressed() -> void:
